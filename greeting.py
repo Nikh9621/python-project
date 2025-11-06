a842da7b00e21fafdf3ed7d97a10cc3c184d855f
Nikh9621 <triloksinghkeshari@gmail.com>
2025-11-06 19:58:42 +0530

    Delete greeting.py
    delete

--- greeting.py
+++
@@ -1,9 +0,0 @@
-print("********************Greeting according to Time***********************")
-print("\n")
-import time
-print("Enter the time:")
-timestamp=time.strftime('%H:%M:%S')
-hours=int (time.strftime('%H'))
-print(timestamp)
-if(time.strftime('%H')<=20 & time.strftime('%H')>=23):
-    print
